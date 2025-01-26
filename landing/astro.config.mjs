47ef53e52625a98c140d630f94a439aaa76db146
Latent <guillaume.thibault.98@proton.me>
2025-01-26 13:03:12 -0500

    chore(landing): Layout

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ]
+});
