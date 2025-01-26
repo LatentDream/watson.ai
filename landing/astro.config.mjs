eb7074d7dec334ce5da3328ea98dc9ed1ccfc536
Latent <guillaume.thibault.98@proton.me>
2025-01-26 16:22:53 -0500

    ci: no need of SSR
    test

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -1,34 +1,20 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-import cloudflare from '@astrojs/cloudflare';
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [
-    react(),
+    react({
+      include: ['**/react/*']
+    }),
     tailwind({
       applyBaseStyles: false,
     }),
   ],
-
   vite: {
     ssr: {
-      external: ['svgo', '@iconify/tools']
-    },
-    resolve: {
-      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
-      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
-      // @ts-ignore
-      alias: import.meta.env.PROD && {
-        "react-dom/server": "react-dom/server.edge",
-      },
+      external: ['svgo', '@iconify/tools'],
+      noExternal: ['react', 'react-dom']
     },
   },
-
-  output: "server",
-  adapter: cloudflare()
 });
